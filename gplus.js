1791bca4b22992a482612b4c161677bacfecc4ee
Geovanna Bamberg <geovanna.t.bamberg@gmail.com>
2022-07-19 20:50:04 -0300

    estilo base atualizado

---
+++ gplus.js
@@ -0,0 +1,13 @@
+function play(){
+    var url = document.getElementById("url").value;
+    var adcionarNaLista = document.getElementById("videos")
+
+    url= url.replace('https://youtu.be/', 'https://www.youtube.com/embed/');
+
+    adcionarNaLista.insertAdjacentHTML("afterbegin", `<h3> VIDEO </h3><iframe class="configVideo" width="350" height="215" src="${url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `)
+}
+
+/*function ErroDeVideosRepetidos(params) {
+    
+} */
--- gplus/gplus.js
+++
@@ -1,9 +0,0 @@
-function play(){
-    var url = document.getElementById("url").value;
-    var adcionarNaLista = document.getElementById("videos")
-
-    url= url.replace('https://youtu.be/', 'https://www.youtube.com/embed/');
-
-    adcionarNaLista.insertAdjacentHTML("afterbegin", `<iframe class="configVideo" width="350" height="215" src="${url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
-}
-
