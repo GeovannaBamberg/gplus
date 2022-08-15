5467e6dbb8a5e8f25f8352ee8206ee1b0ff7a7c2
Geovanna Bamberg <geovanna.t.bamberg@gmail.com>
2022-08-15 08:56:28 -0300

    botao de addicionar e cores novas

--- gplus.js
+++ gplus.js
@@ -2,7 +2,7 @@ var listaDeVideos = []
 function play(){
     var url = document.getElementById("url").value;
     var elementoListaVideos = document.getElementById("videos")
-
+    var elementoBordarPlayer = document.getElementById("bordarPlayer");
     var ajeitandoUrl= url.replace('https://youtu.be/', 'https://www.youtube.com/embed/');
  
     if (ErroDeVideosRepetidos(ajeitandoUrl) == -1) {
@@ -14,6 +14,8 @@ function play(){
         alert("este video ja foi adicionado")
 
     }
+    
+    elementoBordarPlayer.style.display= 'none'
 }
 function ErroDeVideosRepetidos(urlVideos) {
     return listaDeVideos.indexOf(urlVideos)
@@ -21,4 +23,8 @@ function ErroDeVideosRepetidos(urlVideos) {
 function mostrarVideosNaLista(elementoListaVideos,urlVideos) {
     return elementoListaVideos.insertAdjacentHTML("afterbegin", `<iframe class="configVideo" width="350" height="215" src="${urlVideos}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     `)
+}
+function layoutAdicionarVideo(elementoBordarPlayer){
+    var elementoBordarPlayer = document.getElementById("bordarPlayer");
+    elementoBordarPlayer.style.display= 'block'
 }
