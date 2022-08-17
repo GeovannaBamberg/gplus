02ea06d6458c5ac9df7b70897e281c5ce844c3de
Geovanna Bamberg <geovanna.t.bamberg@gmail.com>
2022-08-17 08:46:17 -0300

    erro de links diferentes resolvido

--- gplus.js
+++ gplus.js
@@ -1,18 +1,17 @@
-var listaDeVideos = []
+var listaDeVideos = ['']
 
 function play(){
     var url = document.getElementById("url").value;
     var elementoListaVideos = document.getElementById("videos")
-    var ajeitandoUrl= url.replace('https://youtu.be/', 'https://www.youtube.com/embed/');
+    var novaUrl=ajeitandoUrl(url)
  
-    if (ErroDeVideosRepetidos(ajeitandoUrl) == -1) {
+    if (ErroDeVideosRepetidos(novaUrl) == -1) {
 
-        listaDeVideos.push(ajeitandoUrl)
-        mostrarVideosNaLista(elementoListaVideos,ajeitandoUrl)
+        listaDeVideos.push(novaUrl)
+        mostrarVideosNaLista(elementoListaVideos,novaUrl)
         layoutAdicionarVideo()
-
     } else {
-        alert("este video ja foi adicionado")
+        alert("[ERRO] este video não pode ser adicionado")
     }
 }
 function ErroDeVideosRepetidos(urlVideos) {
@@ -26,4 +25,20 @@ function layoutAdicionarVideo(){
   const elementoBordarPlayer = document.querySelector('section#bordarPlayer')
 
   elementoBordarPlayer.className==='bordarPlayerAberta'? elementoBordarPlayer.className='bordarPlayerFechada':elementoBordarPlayer.className='bordarPlayerAberta'
+}
+
+function ajeitandoUrl(url) {
+    var urlFoco = url
+    var posicao= urlFoco.search("https://youtu.be/")
+    if (posicao==-1) {
+        posicao= urlFoco.search("https://www.youtube.com/")
+
+        if (posicao==-1) {
+            alert("este video não é do youtube")
+        }else{
+            return url.replace('https://www.youtube.com/', 'https://www.youtube.com/embed/');
+        }
+    }else{
+        return url.replace('https://youtu.be/', 'https://www.youtube.com/embed/');
+    }
 }
