85df5c9c9b3afd7b271fd885c0c1102ebe3dc69e
Geovanna Bamberg <geovanna.t.bamberg@gmail.com>
2022-08-17 08:13:44 -0300

    botao on/off adicionado

--- gplus.js
+++ gplus.js
@@ -1,21 +1,19 @@
 var listaDeVideos = []
+
 function play(){
     var url = document.getElementById("url").value;
     var elementoListaVideos = document.getElementById("videos")
-    var elementoBordarPlayer = document.getElementById("bordarPlayer");
     var ajeitandoUrl= url.replace('https://youtu.be/', 'https://www.youtube.com/embed/');
  
     if (ErroDeVideosRepetidos(ajeitandoUrl) == -1) {
-    
+
         listaDeVideos.push(ajeitandoUrl)
         mostrarVideosNaLista(elementoListaVideos,ajeitandoUrl)
-        
+        layoutAdicionarVideo()
+
     } else {
         alert("este video ja foi adicionado")
-
     }
-    
-    elementoBordarPlayer.style.display= 'none'
 }
 function ErroDeVideosRepetidos(urlVideos) {
     return listaDeVideos.indexOf(urlVideos)
@@ -24,7 +22,8 @@ function mostrarVideosNaLista(elementoListaVideos,urlVideos) {
     return elementoListaVideos.insertAdjacentHTML("afterbegin", `<iframe class="configVideo" width="350" height="215" src="${urlVideos}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     `)
 }
-function layoutAdicionarVideo(elementoBordarPlayer){
-    var elementoBordarPlayer = document.getElementById("bordarPlayer");
-    elementoBordarPlayer.style.display= 'block'
+function layoutAdicionarVideo(){
+  const elementoBordarPlayer = document.querySelector('section#bordarPlayer')
+
+  elementoBordarPlayer.className==='bordarPlayerAberta'? elementoBordarPlayer.className='bordarPlayerFechada':elementoBordarPlayer.className='bordarPlayerAberta'
 }
